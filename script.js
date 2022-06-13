7d6b331ef9ee580229fed60f43d587591919db33
Amber Leigh <amberbiggart55@gmail.com>
2022-06-12 22:06:27 -0600

    added functionality to the game counter

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 let displayChoices = document.getElementById("display-choices");
 let displayWinner = document.getElementById("display-winner");
 let displayCompChoice = document.getElementById("comp-choice");
-
+let displayGamesPlayed = document.getElementById("display-games-played");
+let displayGamesWon = document.getElementById("display-games-won");
+let displayGamesLost = document.getElementById("display-games-lost");
+let displayGameTied = document.getElementById("display-games-tied");
 
 // user's choice
 const userObj = {
+    wins: 0,
+    loss: 0,
+    ties: 0,
+
     _choice: "",
     askUserChoice(){
         let userChoice = "";
@@ -39,21 +46,32 @@ const compObj = {
 
 // compare user choice vs computer choice
 const gameObj = {
-
+    gameCounter:0,
 
     playGame(event) {
+        this.gameCounter++
 
+        displayGamesPlayed.innerHTML
         let userChoice = event.target.value;
         let computerChoice = compObj.getComputerChoice();
         displayCompChoice.innerHTML = `${computerChoice}`;
         displayChoices.innerHTML = `${userChoice} VS. ${computerChoice}`;
         if ((userChoice === "Rock" && compChoice === "scissors") || (userChoice === "paper" && computerChoice === "rock") || (userChoice === "scissors" && computerChoice === "paper")) {
             displayWinner.innerHTML = `Your ${userChoice} won!`;
+            userObj.wins++;
         } else if (userChoice === computerChoice) {
             displayWinner.innerHTML = `Your ${userChoice} tied.`;
+            userObj.ties++;
         } else {
             displayWinner.innerHTML= `Your ${userChoice} lost to ${computerChoice}.`
+            userObj.loss++;
         }
+
+        displayGamesPlayed.innerHTML = this.gameCounter;
+        displayGamesWon.innerHTML = userObj.wins;
+        displayGamesLost.innerHTML = userObj.loss;
+        displayGameTied.innerHTML = userObj.ties;
+
     }
 
 
